6f650bb4789742c4efd7605e973b8f1780f43f29
abolzzz <82838037+abolzzz@users.noreply.github.com>
2022-05-18 23:16:44 +0300

    Add files via upload

---
+++ precache-manifest.07858fe20ab427c7aa91b9234f845dcf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "13367d9558b3553c4677",
+    "url": "/static/css/main.6c557ce8.chunk.css"
+  },
+  {
+    "revision": "13367d9558b3553c4677",
+    "url": "/static/js/main.13367d95.chunk.js"
+  },
+  {
+    "revision": "ac2e7b5deb3dac76bf0e",
+    "url": "/static/css/1.371b8a45.chunk.css"
+  },
+  {
+    "revision": "ac2e7b5deb3dac76bf0e",
+    "url": "/static/js/1.ac2e7b5d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a0d774d2b07e683e7926899afb3ebb66",
+    "url": "/static/media/me6.a0d774d2.png"
+  },
+  {
+    "revision": "24b802b0d3310567dcf587228c10fb9a",
+    "url": "/static/media/pepega.24b802b0.jpg"
+  },
+  {
+    "revision": "a04855aa0e38a6e8c4b175c2318b6fad",
+    "url": "/static/media/profile.a04855aa.jpg"
+  },
+  {
+    "revision": "bfbbb8a4cae2784a9622961145f11c15",
+    "url": "/static/media/profile5.bfbbb8a4.jpg"
+  },
+  {
+    "revision": "ddf249605ad1c1c58cd74d07ae52bd57",
+    "url": "/static/media/profile3.ddf24960.jpg"
+  },
+  {
+    "revision": "c685367f1406e6abdd37a121d7c2f4c2",
+    "url": "/static/media/profile4.c685367f.jpg"
+  },
+  {
+    "revision": "c14f3db531d12277a5d9efcd103adfa1",
+    "url": "/index.html"
+  }
+];
